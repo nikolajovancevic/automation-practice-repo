c0577b97634da181ace87f0967f5b965a7dfa535
nikolajovancevic <nikol4.dzoni@gmail.com>
2024-04-23 17:03:07 +0200

    fix tests

--- e2e/landing-page.test.ts
+++ e2e/landing-page.test.ts
@@ -4,7 +4,7 @@ test("Check Landing page text", async ({ page }) => {
   await page.goto("/");
 
   await expect(page.getByTestId("page-title-text")).toHaveText(
-    "DEVELOPMENT ENVIRONMENT test",
+    "DEVELOPMENT ENVIRONMENT",
   );
   await expect(page.getByTestId("description-text")).toHaveText(
     "This environment is used to practice deployment via GitActions, as well as ci/cd flow in general.",
