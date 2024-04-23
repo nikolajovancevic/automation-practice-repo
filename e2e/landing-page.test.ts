6338de1ab04e757458dc2a4b36776780c167f436
nikolajovancevic <nikol4.dzoni@gmail.com>
2024-04-23 13:45:07 +0200

    test

--- e2e/landing-page.test.ts
+++ e2e/landing-page.test.ts
@@ -1,9 +1,13 @@
 import { test, expect } from "@playwright/test";
 
 test("Check Landing page text", async ({ page }) => {
-  await page.waitForTimeout(10000)
-  console.log("timeout expired")
   await page.goto("https://bloxico.com/");
+  
+  await page.waitForTimeout(1000)
+  console.log("timeout expired")
+  await page.goto("https://localhost:3000");
+  await page.waitForTimeout(1000)
+
 
   await expect(page.getByTestId("page-title-text")).toHaveText(
     "DEVELOPMENT ENVIRONMENT",
