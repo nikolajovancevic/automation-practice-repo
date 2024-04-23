7a61cf2205b8c071f1c2f91c3dd9787b1f590567
nikolajovancevic <nikol4.dzoni@gmail.com>
2024-04-23 13:49:07 +0200

    add url

--- e2e/landing-page.test.ts
+++ e2e/landing-page.test.ts
@@ -4,13 +4,15 @@ test("Check Landing page text", async ({ page }) => {
   await page.goto("https://bloxico.com/");
   
   await page.waitForTimeout(1000)
-  console.log("timeout expired")
-  await page.goto("https://localhost:3000");
+  console.log("opend bloxico")
+  await page.goto("https://localhost:3000/");
+  console.log("opend localhost")
+
   await page.waitForTimeout(1000)
 
 
   await expect(page.getByTestId("page-title-text")).toHaveText(
-    "DEVELOPMENT ENVIRONMENT",
+    "DEVELOPMENT ENVIRONMENT test",
   );
   await expect(page.getByTestId("description-text")).toHaveText(
     "This environment is used to practice deployment via GitActions, as well as ci/cd flow in general.",
