dad90e83383afada09aec012fe72d1401a0c4a6f
nikolajovancevic <nikol4.dzoni@gmail.com>
2024-04-23 15:16:06 +0200

    wrong url

--- e2e/landing-page.test.ts
+++ e2e/landing-page.test.ts
@@ -1,15 +1,7 @@
 import { test, expect } from "@playwright/test";
 
 test("Check Landing page text", async ({ page }) => {
-  await page.goto("https://bloxico.com/");
-  
-  await page.waitForTimeout(1000)
-  console.log("opend bloxico")
-  await page.goto("http://localhost:3000/");
-  console.log("opend localhost")
-
-  await page.waitForTimeout(1000)
-
+  await page.goto("/");
 
   await expect(page.getByTestId("page-title-text")).toHaveText(
     "DEVELOPMENT ENVIRONMENT test",
