5c7f2fd1c800200d02335b23cb3a3cacb3073456
nikolajovancevic <nikol4.dzoni@gmail.com>
2024-04-23 13:32:15 +0200

    add artifacts

--- playwright.config.ts
+++ playwright.config.ts
@@ -18,7 +18,9 @@ export default defineConfig({
     baseURL: "http://localhost:3000/",
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: "on-first-retry",
+    trace: "on",
+    video: "on",
+    screenshot: "on"
   },
 
   /* Configure projects for major browsers */
