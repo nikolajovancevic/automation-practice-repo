005679bc07f7d898fb07f18d4798003d939feb47
nikolajovancevic <nikol4.dzoni@gmail.com>
2024-04-25 16:58:43 +0200

    updated tests and base url

--- e2e/landing-page.test.ts
+++ e2e/landing-page.test.ts
@@ -4,9 +4,9 @@ test("Check Landing page text", async ({ page }) => {
   await page.goto("/");
 
   await expect(page.getByTestId("page-title-text")).toHaveText(
-    "DEVELOPMENT ENVIRONMENT",
+    "STAGING ENVIRONMENT"
   );
   await expect(page.getByTestId("description-text")).toHaveText(
-    "This environment is used to practice deployment via GitActions, as well as ci/cd flow in general.",
+    "This environment is used to practice deployment via GitActions, as well as ci/cd flow in general."
   );
 });
