2311db573172110dd612ffb83c38e7154b5898c5
nikolajovancevic <nikol4.dzoni@gmail.com>
2024-05-13 17:38:52 +0200

    refactor: fix test, and fe desc

--- e2e/landing-page.test.ts
+++ e2e/landing-page.test.ts
@@ -1,12 +1,12 @@
 import { test, expect } from "@playwright/test";
 
 test("Check Landing page text", async ({ page }) => {
-  await page.goto("/automating-stuff/");
+  await page.goto("/");
 
   await expect(page.getByTestId("test-env-header")).toHaveText(
     "Test Environment"
   );
   await expect(page.getByTestId("description-text")).toHaveText(
-    "Dzon's test environment for practicing CI/CD, GitActions, semantic release, etc."
+    "Dzon's environment used to practice deployment via GitActions, as well as CI/CD flow."
   );
 });
