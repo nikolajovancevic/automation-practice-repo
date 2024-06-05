e167260436df9d6a078b26d6f25e4b56ac95d797
nikolajovancevic <nikol4.dzoni@gmail.com>
2024-06-05 13:24:22 +0200

    fix: fixing workflows and stuff

--- e2e/landing-page.test.ts
+++ e2e/landing-page.test.ts
@@ -6,7 +6,5 @@ test("Check Landing page text", async ({ page }) => {
   await expect(page.getByTestId("test-env-header")).toHaveText(
     "Test Environment",
   );
-  await expect(page.getByTestId("description-text")).toHaveText(
-    "Dzon's test environment for practicing CI/CD, GitActions, semantic release, etc.",
-  );
+  await expect(page.url()).toContain("automating-stuff/")
 });
