8d5117e5153ce30d2eca910bd3c598a4eb747060
nikolajovancevic <nikol4.dzoni@gmail.com>
2024-12-27 10:24:22 +0100

    feat: new page look

--- fe/scripts.js
+++ fe/scripts.js
@@ -1,16 +1,3 @@
-// Hamburger menu
-const hamburger = document.querySelector(".hamburger");
-
-if (hamburger) {
-  // Add a click event listener
-  hamburger.addEventListener("click", () => {
-    console.log("Hamburger clicked!"); // Debugging log
-    hamburger.classList.toggle("is-active"); // Toggle class
-  });
-} else {
-  console.error("Hamburger button not found!");
-}
-
 // Get app version from package.json
 fetch("package.json")
   .then((response) => response.json())
@@ -18,35 +5,6 @@ fetch("package.json")
     // Extract version from package.json
     const version = data.version;
     // Update the HTML element with the version
-    document.getElementById("version").textContent = version;
+    document.getElementById("version").textContent = `v${version}`;
   })
   .catch((error) => console.error("Error fetching package.json:", error));
-
-// Counter for break button
-
-function breakCounter() {
-  let counter = parseInt(sessionStorage.getItem("counter")) || 0;
-  const breakButton = document.getElementById("break-button");
-  const breakCounter = document.getElementById("break-counter");
-
-  breakButton.addEventListener("click", function(e) {
-    // Prevent immediate redirection
-    e.preventDefault();
-
-    counter++;
-    breakCounter.textContent = counter;
-
-    sessionStorage.setItem("counter", counter);
-
-    // Add a class to trigger the button expansion animation
-    this.classList.add('expanding');
-
-    // Wait for 2 seconds (duration of the animation), then redirect
-    setTimeout(() => {
-      // Perform redirection after the animation
-      window.location.href = this.href;
-    }, 1000);
-  });
-}
-
-breakCounter();
